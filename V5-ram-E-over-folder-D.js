fe0c14ce595c15e9d22cfcf7014afc45e2266a54
alejandro <diazbrandonalejandro@gmail.com>
2023-01-24 23:48:27 +0100

    getfolder para destinos falla

---
+++ V5-ram-E-over-folder-D.js
@@ -0,0 +1,53 @@
+var rutaDestino = "C:/MASTER ADVANCED/D/";
+var rutaOrigen = "C:/MASTER ADVANCED/E/";
+var rutaCopia = "D:/MASTER ADVANCED/C plus D";
+var combinaciones = [];
+var contador = 0;
+
+// obtenemos todos los archivos de origen de la carpeta
+var archivosOrigen = new Folder(rutaOrigen).getFiles();
+var archivosDestino = new Folder(rutaDestino).getFiles();
+
+while(contador < 100) {
+    for (var i = 0; i < archivosDestino.length; i++) {
+        var numAlAzar = Math.floor(Math.random() * archivosOrigen.length);
+        while(archivosOrigen[numAlAzar] === archivosDestino[i]){
+            numAlAzar = Math.floor(Math.random() * archivosOrigen.length);
+        }
+        var utilizado = false;
+        var combinacion = archivosOrigen[numAlAzar].toString() + "-" + archivosDestino[i].toString();
+
+        for (var j = 0; j < combinaciones.length; j++){
+            if(combinaciones[j] === combinacion){
+                utilizado = true;
+                break;
+            }
+        }
+        if(!utilizado){
+            //Buscamos al azar y copiamos la capa activa.
+            var docOrigen = app.open(archivosOrigen[numAlAzar]);
+            docOrigen.activeLayer.copy();
+            //
+            var docDestino = app.open(archivosDestino[i]);
+            docDestino.paste();
+            //generamos trazabilidad
+            var nombreArchivoOrigen = archivosOrigen[numAlAzar].toString().split("/").pop().split(".")[0];
+            var nombreArchivoDestino = archivosDestino[i].toString().split("/").pop().split(".")[0];
+            var nombreCopia = nombreArchivoOrigen + "-" + nombreArchivoDestino + ".psd";
+            //se genera nuevo archivo
+            var copia = new File(rutaCopia + "/" + nombreCopia);
+            docDestino.saveAs(copia);
+            //cerramos conexiones y limpiamos variables
+            docDestino.close(SaveOptions.DONOTSAVECHANGES);
+            docOrigen.close(SaveOptions.DONOTSAVECHANGES);
+            docOrigen = null;
+            docDestino = null;
+            combinaciones.push(combinacion);
+            archivosOrigen.splice(numAlAzar, 1);
+            contador++;
+            if(contador === 100){
+            break;
+        }
+    }
+}
+}
