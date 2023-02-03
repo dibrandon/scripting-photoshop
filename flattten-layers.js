94891903219e74d91e25855be27a3aa70b7a9d92
alejandro <diazbrandonalejandro@gmail.com>
2023-02-03 23:03:02 +0100

    nuevo script para combinar todos los layers visibles

---
+++ flattten-layers.js
@@ -0,0 +1,25 @@
+var folder = new Folder("D:/PILAS ORIGIN MASTER - ORIGINAL SOLO PARA DAR COLOR");
+var fileList = folder.getFiles();
+
+for (var i = 0; i < fileList.length; i++) {
+  var file = fileList[i];
+  if (file instanceof File && file.name.match(/\.(psd)$/i)) {
+    app.open(file);
+    var activeDocument = app.activeDocument;
+    var layers = activeDocument.layers;
+
+    var numberOfLayers = layers.length;
+
+    for (var j = 0; j < numberOfLayers; j++) {
+      layers[j].visible = true;
+    }
+
+    var transparentLayer = activeDocument.artLayers.add();
+    transparentLayer.name = "Calque 1";
+    transparentLayer.move(activeDocument, ElementPlacement.PLACEATBEGINNING);
+
+    activeDocument.mergeVisibleLayers();
+    activeDocument.save();
+    activeDocument.close();
+  }
+}
